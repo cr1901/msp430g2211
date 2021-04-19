68e45a943d81359a45cfbab3c3d303d3c2021f15
William D. Jones <thor0505@comcast.net>
2021-04-18 21:21:26 -0400

    Regenerate crate with svd2rust v0.18.0 and msp430_svd fixes.

--- src/timer_a2.rs
+++ src/timer_a2.rs
@@ -18,6 +18,10 @@ pub struct RegisterBlock {
     #[doc = "0x46 - Timer A Capture/Compare 1"]
     pub taccr1: crate::Reg<taccr1::TACCR1_SPEC>,
 }
+#[doc = "TAIV register accessor: an alias for `Reg<TAIV_SPEC>`"]
+pub type TAIV = crate::Reg<taiv::TAIV_SPEC>;
+#[doc = "Timer A Interrupt Vector Word"]
+pub mod taiv;
 #[doc = "TACTL register accessor: an alias for `Reg<TACTL_SPEC>`"]
 pub type TACTL = crate::Reg<tactl::TACTL_SPEC>;
 #[doc = "Timer A Control"]
@@ -30,10 +34,6 @@ pub mod tacctl0;
 pub type TACCTL1 = crate::Reg<tacctl1::TACCTL1_SPEC>;
 #[doc = "Timer A Capture/Compare Control 1"]
 pub mod tacctl1;
-#[doc = "TAIV register accessor: an alias for `Reg<TAIV_SPEC>`"]
-pub type TAIV = crate::Reg<taiv::TAIV_SPEC>;
-#[doc = "Timer A Interrupt Vector Word"]
-pub mod taiv;
 #[doc = "TAR register accessor: an alias for `Reg<TAR_SPEC>`"]
 pub type TAR = crate::Reg<tar::TAR_SPEC>;
 #[doc = "Timer A Counter Register"]
